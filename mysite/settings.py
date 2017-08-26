76ed1c1532ef7f9f12b9b15190798270fcc209c8
alice <aliceturino@gmail.com>
2017-08-26 14:23:56 -0300

    consertando settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$@g_^sqhs7d)=tg_ao-%ms^@o#kvtmjzk0mb4swzjtw7p$5(rx'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
